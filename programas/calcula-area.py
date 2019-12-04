84a0703e0e1d43b4ea5ff6413fd1094ccb9439ba
LaMenteOcupada <lamenteocupada@gmail.com>
2019-12-04 08:29:51 +0100

    Initial commit

---
+++ programas/calcula-area.py
@@ -0,0 +1,8 @@
+# nombre = input('Dime tu nombre:')
+# edad = input('...y tu edad: ')
+# print(nombre, ' y tienes ' , edad ,' años.')
+
+#Calcula el area de un circulo
+radio = input('Introduce el radio del circulo(m):')
+area = 3.142*int(radio)**2
+print('El area del círculo es: ',area)
