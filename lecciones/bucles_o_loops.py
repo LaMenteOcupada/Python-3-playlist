f62138024f6e5c86b58bbf559e1bedf6963d2d7e
LaMenteOcupada <lamenteocupada@gmail.com>
2019-12-24 16:25:04 +0100

    Lección 10

--- lecciones/bucles_o_loops.py
+++ lecciones/bucles_o_loops.py
@@ -1,14 +1,29 @@
-filosofos = ['Marx','Mileto','Platón','Sócrates']
+#BUCLES FOR
+# filosofos = ['Marx','Mileto','Platón','Sócrates']
+#
+# #for filosofo in filosofos:
+# #    print(filosofo)
+#
+# #for filosofo in filosofos[1:3]:
+# #    print (filosofo)
+#
+# for filosofo in filosofos:
+#     if filosofo == 'Platón':
+#         print(f'{filosofo} - es el mejor.')
+#         break
+#     else:
+#         print(filosofo)
 
-#for filosofo in filosofos:
-#    print(filosofo)
+#BUCLES WHILE
+edad = 30
+num = 0
 
-#for filosofo in filosofos[1:3]:
-#    print (filosofo)
-
-for filosofo in filosofos:
-    if filosofo == 'Platón':
-        print(f'{filosofo} - es el mejor.')
+while num < edad:
+    if num == 0:
+        num += 1
+        continue
+    if num % 2 == 0:
+        print (num)
+    if num > 10:
         break
-    else:
-        print(filosofo)
+    num += 1
