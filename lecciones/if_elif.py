011dff1d6acee0f6e4f66592147a1b8cbf4f493f
LaMenteOcupada <lamenteocupada@gmail.com>
2019-12-24 12:47:38 +0100

    Lección 8

---
+++ lecciones/if_elif.py
@@ -0,0 +1,19 @@
+#OPERADORES
+#<,>,==,!=,=>,=<
+
+# edad = int(input('Introduce tu edad:'))
+#
+# if edad < 10:
+#     #nuestro código
+#     print('Eres joven!')
+# elif edad < 50:
+#     print('Vaya, tienes menos de 50!')
+# else:
+#     print('Eres sabio, sin duda,forastero!')
+
+carnivoro = input('Comes carne?(s/n):')
+
+if carnivoro == 's':
+    print('Aquí tiene el menú de carnes..')
+else:
+    print('Aquí tiene el menú vegano/vegetariano..')
