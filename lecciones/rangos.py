9680eb9c5bb7ba3b564b738d48a38d924bbae5e5
LaMenteOcupada <lamenteocupada@gmail.com>
2020-02-09 12:28:59 +0100

    Lección 11

---
+++ lecciones/rangos.py
@@ -0,0 +1,19 @@
+# for n in range(6):
+#     #haz algo
+#     print(n)
+
+# for n in range(3,11):
+#     #haz algo
+#     print(n)
+
+# for n in range(0,21,4):
+#     #haz algo
+#     print(n)
+
+hamburguesas=['carne','pollo','suprema','doble']
+#
+# for n in range(len(hamburguesas)):
+#     print(n,hamburguesas[n])
+
+for n in range(len(hamburguesas)-1, -1, -1 ):
+    print(n,hamburguesas[n])
