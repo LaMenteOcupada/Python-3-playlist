1f3d07d4272caab8b86de120e9f43dff6af509d4
LaMenteOcupada <lamenteocupada@gmail.com>
2020-02-09 13:13:46 +0100

    Lección 12

---
+++ lecciones/funciones.py
@@ -0,0 +1,12 @@
+def area(radio):
+    return (3.142*radio*radio)
+
+def vol(area,longitud):
+    print(area*longitud)
+
+radio = int(input('Introduce el radio:'))
+longitud = int(input('Introduce la longitud:'))
+
+
+#area_calc = 
+vol(area(radio),longitud)
